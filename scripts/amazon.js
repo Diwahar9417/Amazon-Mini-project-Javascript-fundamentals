ba72628df4fb02ae89a758358cbc3a61de135c66
Diwahar <diwahar9417@gmail.com>
2024-11-28 16:12:21 +0530

    Make the cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -66,15 +66,20 @@ document.querySelectorAll(`.js-add-to-cart`).forEach((button)=>{
     }
    });
    if(matchingItem){
-    matchingItem.Quantity+=1;
+    matchingItem.quantity+=1;
    }
    else{
     cart.push({
       productId: productId,
-      Quantity: 1
+      quantity: 1
      });
    }
+  let cartQuantity = 0;
+
+  cart.forEach((item)=>{
+    cartQuantity += item.quantity;
+  });
+  document.querySelector(`.js-cart-quantity`).innerHTML= cartQuantity;
   
-   console.log(cart);
   });
 })
