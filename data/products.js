0ddf7bf2cdbff2a397c193d2abd40570b1cd168d
Diwahar <diwahar9417@gmail.com>
2024-12-19 04:12:05 +0530

    Use inertance and polymorphism to add size chart

--- data/products.js
+++ data/products.js
@@ -31,23 +31,31 @@ class product{
    getPrice(){
     return `$${formatCurrency(this.priceCents)}`;
    }
+
+   extraInfoHTML(){
+    return ``;
+   }
 }
 
-const product1 = new product({
-  id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-  image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-  name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-  rating: {
-    stars: 4.5,
-    count: 87
-  },
-  priceCents: 1090,
-  keywords: [
-    "socks",
-    "sports",
-    "apparel"
-  ]
-});
+class Clothing extends product {
+  sizeChartLink;
+
+  constructor(productDetails){
+    super(productDetails)
+    this.sizeChartLink = productDetails.sizeChartLink;
+  }
+
+  extraInfoHTML(){
+    // super.extraInfoHTML();
+    return `
+      <a href ="${this.sizeChartLink}" target="_blank">
+      Size chart
+      </a>
+    `;
+  }
+
+}
+
 
 export const products = [
   {
@@ -709,5 +717,8 @@ export const products = [
     ]
   }
 ].map((productDetails) => {
+  if (productDetails.type === 'clothing'){
+    return new Clothing (productDetails);
+  }
   return new product(productDetails);
 });
