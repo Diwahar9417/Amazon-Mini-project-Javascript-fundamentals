02748662039692a7a92c31e2a7bce9bd40f9d823
Diwahar <diwahar9417@gmail.com>
2024-12-19 04:34:14 +0530

    Learn more details about classes and this

--- data/products.js
+++ data/products.js
@@ -55,6 +55,26 @@ class Clothing extends product {
   }
 
 }
+/*
+const date = new Date();
+console.log(date);
+console.log(date.toLocaleTimeString());
+*/
+
+/*
+const object2= {
+  a:2,
+  b: this.a
+};
+
+
+function logThis(){
+  console.log(this);
+}
+logThis();
+logThis.call(`hello`);
+
+*/
 
 
 export const products = [
